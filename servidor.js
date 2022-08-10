4dca9947ae228283162fe8c0b6856f004b9ac7bd
unknown <erickspy2003@gmail.com>
2022-08-10 19:38:13 -0300

    added a new request

--- servidor.js
+++ servidor.js
@@ -2,9 +2,22 @@ const porta = 3003
 
 const express = require("express")
 const app = express()
+const banco = require('./banco.js')
 
 app.get('/produtos', (req, res, next) => {
-    res.send({ Nome:  'notebook', Preco: 123.45, quantidade: 50 }) //converter to JSON
+    res.send(banco.getProdutos()) //converter to JSON
+})
+
+app.get('/produtos/:id', (req, res, next) => {
+    res.send(banco.getProduto(req.params.id))
+})
+
+app.post('/produtos', (req, res, next) => {
+    const produto = banco.salvarProduto({
+        nome: req.body.name,
+        preco: req.body.preco
+    })
+    res.send(produto)
 })
 
 app.listen(porta, () => {
